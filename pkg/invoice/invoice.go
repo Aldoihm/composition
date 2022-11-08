0dc19ef4af557b5ffb2587711c57925a608aa376
Aldo Predator <ingaldo@sotecmovil.com>
2022-11-08 00:15:03 -0600

    se crea el constructor de invoice y un método para sumar el total de los items

--- pkg/invoice/invoice.go
+++ pkg/invoice/invoice.go
@@ -5,10 +5,23 @@ import (
 	"github.com/Aldoihm/composition/pkg/invoiceitem"
 )
 
+// Invoice is the struct of a invoice
 type Invoice struct {
 	country string
 	city    string
 	total   float64
 	client  customer.Customer
-	intem   []invoiceitem.Item
+	items   []invoiceitem.Item
+}
+
+// SetTotal is the setter of Invoice.total
+func (i *Invoice) SetTotal() {
+	for _, valor := range i.items {
+		i.total += valor.Value()
+	}
+}
+
+// constructor, returns a new Invoice
+func New(country, city string, client customer.Customer, items []invoiceitem.Item) Invoice {
+	return Invoice{country: country, city: city, client: client, items: items}
 }
