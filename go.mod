82db553732aa68aaf61cd3fdc6ba061e503baa4a
Aldo Predator <ingaldo@sotecmovil.com>
2022-11-07 23:42:14 -0600

    se inicializa el módulo y se crea la relacion 1 a 1 con invoice y customer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aldoihm/composition
+
+go 1.19
--- pkg/customer/customer.go
+++ pkg/customer/customer.go
@@ -1,1 +1,12 @@
 package customer
+
+type Customer struct {
+	name    string
+	address string
+	phone   string
+}
+
+// constructor de nuestro cliente
+func New(name, address, phone string) Customer {
+	return Customer{name, address, phone}
+}
