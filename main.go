aff9158d63ecd03daa0d83ae1b0a44be5216dee2
Aldo Predator <ingaldo@sotecmovil.com>
2022-11-08 09:20:14 -0600

    Ejemplo terminado de composición en go, que es la sustitución de la herencia en otros lenguajes de programación

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/Aldoihm/composition/pkg/customer"
+	"github.com/Aldoihm/composition/pkg/invoice"
+	"github.com/Aldoihm/composition/pkg/invoiceitem"
+)
+
+func main() {
+	items := []invoiceitem.Item{
+		invoiceitem.New(1, "papas sabritas adobadas", 10.5),
+		invoiceitem.New(2, "chicle", 3.5),
+		invoiceitem.New(3, "coca 1.750ml", 22),
+	}
+	invoice := invoice.New(
+		"México",
+		"Teziutlán",
+		customer.New("Aldo", "Av. Morelos", "2313228244"),
+		items,
+	)
+	invoice.SetTotal()
+	fmt.Printf("%+v", invoice)
 }
