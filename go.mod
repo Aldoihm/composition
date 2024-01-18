bff67dbc4289febfeaa4d833543711b4822a0a3b
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-01-18 09:36:35 -0600

    Se crea la estructura de cliente, de factura y el mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aldoihm/composition
+
+go 1.21.2
--- pkg/customer/customer.go
+++ pkg/customer/customer.go
@@ -1,1 +1,13 @@
 package customer
+
+type Customer struct {
+	name    string
+	address string
+	phone   string
+}
+
+//Método que retorna a un nuevo cliente
+
+func New(name string, address string, phone string) Customer {
+	return Customer{name: name, address: address, phone: phone}
+}
